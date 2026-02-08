7656e08ca28022efffbd7b9beca5071241c0e4f3
Donghyun Kim <newdhkim0615@gmail.com>
2026-02-08 19:31:48 +0900

    Initial commit

---
+++ src/main/java/com/konkuk/no_ingredient/NoIngredientApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.no_ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoIngredientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoIngredientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/no_ingredient/NoIngredientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.no_ingredient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoIngredientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
